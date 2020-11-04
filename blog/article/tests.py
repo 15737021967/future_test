f83e810e7478c3583e99cd9b94de9eaeee8a73e0
Jiale <1531391246@qq.com>
2020-11-04 14:46:09 +0800

    补充tests.py文件

---
+++ blog/article/tests.py
@@ -0,0 +1,44 @@
+from blog.tests import BaseTestCase
+from blog.article import models
+from blog.article import repository
+from blog import db
+import unittest
+
+
+class ArticleTest(BaseTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(ArticleTest, cls).setUpClass()
+        category_ins = models.Category(
+            name="test_category"
+        )
+        tag_ins_list = [
+            models.Tag(
+                name="123" + str(i)
+            )
+            for i in range(4)
+        ]
+        db.session.add(category_ins)
+        db.session.add_all(tag_ins_list)
+        db.session.commit()
+        cls.tag_list = [
+            tag.id
+            for tag in tag_ins_list
+        ]
+        cls.category_id = category_ins.id
+        print(str(category_ins.id))
+        n = category_ins.name
+
+    @unittest.skip("")
+    def test_article_add(self):
+        title = "123"
+        describe = "123"
+        content = "123"
+        tags = models.Tag.query.filter(models.Tag.id.in_(self.tag_list))
+        repository.ArticleRepority.article_add(
+            title=title, describe=describe, content=content, tags=tags, category=self.category_id
+        )
+        article_ins: models.Article = models.Article.query.filter(models.Article.title == "123").first()
+        self.assertEqual(article_ins.category_id, self.category_id)
+        self.assertEqual(len(article_ins.tags.all()), len(self.tag_list))
