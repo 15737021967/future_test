f401793c38553ed7acd3138b3e5471a457db302e
Jiale <1531391246@qq.com>
2021-03-02 17:43:42 +0800

    修复一个单元测试的bug

--- tests/base/base.py
+++ tests/base/base.py
@@ -21,28 +21,35 @@ class SampleData:
 
         self.instance = self.model()
 
-        columns = INSPECTOR.get_columns(self.model.__name__.lower())
+        columns = INSPECTOR.get_columns(self.model.__tablename__)
         for column in columns:
             column_name = column.get('name')
             column_nullable = column.get('nullable')
             column_type = column.get("type")
-            column_default = column.get("default")
             if column_name == 'id':
                 continue
             if column_name in self.data:
+                data = self.data.get(column_name)
                 if isinstance(column_type, db.Integer) and \
-                        getattr(self.model, column_name).foreign_keys:
-                    fk = self.data.get(column_name).create_instance()
+                        isinstance(data, SampleData):
+                    fk = data.create_instance()
                     setattr(self.instance, column_name, fk)
                 else:
-                    setattr(self.instance, column_name, self.data.get(column_name))
-            if column_name not in self.data and column_nullable is False and column_default is None:
+                    setattr(self.instance, column_name, data)
+            if column_name not in self.data and column_nullable is False:
                 raise Exception(f'{self.model.__name__.lower()} 的非空字段 {column_name} 不能为空')
 
         db.session.add(self.instance)
         db.session.flush()
 
         return self.instance.id
+
+    def relation_model(self):
+        result = [self]
+        for key, value in self.data.items():
+            if isinstance(value, SampleData):
+                result.extend(value.relation_model())
+        return result
 
     def init_instance(self):
         self.instance = None
@@ -52,6 +59,13 @@ class BaseEnv:
 
     def __init__(self, sample_data_list: List[SampleData]):
         self.sample_data_list = sample_data_list
+        self.relation = []
+        self.init_relation()
+
+    def init_relation(self):
+        for item in self.sample_data_list:
+            self.relation.extend(item.relation_model())
+        self.relation = set(self.relation)
 
     def __enter__(self):
         for ins in self.sample_data_list:
@@ -59,7 +73,7 @@ class BaseEnv:
         db.session.commit()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        for ins in self.sample_data_list:
+        for ins in self.relation:
             ins.init_instance()
         model_list = BaseModel.__subclasses__()
         for model in model_list:
